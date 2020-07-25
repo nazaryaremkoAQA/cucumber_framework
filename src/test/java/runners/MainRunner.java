da3568e412496ab653f3384cbd74d905d123a9ed
nazaryaremkoAQA <nazaryaremko90@gmail.com>
2020-07-25 15:58:47 +0300

    change

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -13,10 +13,10 @@ import pageObjects.BasePage;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features = { "src/test/java/resources/features/" }, glue = {
-		"stepDefinitions" }, monochrome = true, tags = {}, 
-				plugin = { "pretty", "html:target/cucumber", "json:target/cucumber.json",
-				"com.cucumber.listener.ExtentCucumberFormatter:output/report.html" })
+@CucumberOptions(features = {"src/test/java/resources/features/"}, glue = {
+        "stepDefinitions"}, monochrome = true, tags = {},
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json",
+                "com.cucumber.listener.ExtentCucumberFormatter:output/report.html"})
 
 public class MainRunner extends AbstractTestNGCucumberTests {
 	@AfterClass
